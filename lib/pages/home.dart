0054d16dce5299af57bec7a573ca4bad48412b44
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-10-24 19:33:23 +0530

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatelessWidget { 
   @override
   Widget build(BuildContext context) {
     return Container(
