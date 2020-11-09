59c7a7656c38910409743676e0e4bbfb77012ec8
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-09 21:55:48 +0530

    Update SignUpPage.dart

--- lib/pages/SignUpPage.dart
+++ lib/pages/SignUpPage.dart
@@ -9,6 +9,7 @@ class SignUpPage extends StatelessWidget {
     String defaultFontFamily = 'Roboto-Light.ttf';
     double defaultFontSize = 14;
     double defaultIconSize = 17;
+    
 
     return Scaffold(
       body: Container(
