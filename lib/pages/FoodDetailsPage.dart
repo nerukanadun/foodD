adfb1a6485aea68f8a473c54001d40f35a444049
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-09 21:59:10 +0530

    Update FoodDetailsPage.dart

--- lib/pages/FoodDetailsPage.dart
+++ lib/pages/FoodDetailsPage.dart
@@ -324,7 +324,7 @@ class DetailContentMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Text(
-        'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book.',
+        'Lorem ipsum,  or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book.',
         style: TextStyle(
             fontSize: 14.0,
             color: Colors.black87,
