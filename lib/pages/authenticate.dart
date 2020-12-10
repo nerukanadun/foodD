28594e813879094fa7c8e8c2d2938847a2df6dc4
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-12-10 21:46:20 +0530

    Update authenticate.dart

--- lib/pages/authenticate.dart
+++ lib/pages/authenticate.dart
@@ -5,11 +5,7 @@ class Authenticate extends StatefulWidget {
   _AuthenticateState createState() => _AuthenticateState();
 }
 
-class _AuthenticateState extends State<Authenticate> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('authenticate'),
+
     );
   }
   
